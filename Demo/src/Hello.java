204e4182fd056667831f50f96bf0a15cbcb5a02e
Sushil Karkee <sushilkarkee@users.noreply.github.com>
2016-07-19 16:06:35 +0530

    Update Hello.java

--- Demo/src/Hello.java
+++ Demo/src/Hello.java
@@ -6,7 +6,7 @@ public static void main(String[] args) {
 	System.out.println("hello from github again");
         System.out.println("welcome again");
         //this is comment line
-        System.out.println("jptingss");
+        System.out.println("jptingaaa");
 	
 }
 }
