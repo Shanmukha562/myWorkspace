3b82dd57c6dae69405d8f128859697ad64bac6a9
sushil.bahadur.karki <sushil.bahadur.karki@HDC4-D-3B7ZXC2.dir.svc.accenture.com>
2016-07-19 14:52:22 +0530

    first

---
+++ Demo/src/Calculator.java
@@ -0,0 +1,20 @@
+
+public class Calculator {
+	
+	public static void main(String[] args) {
+		
+		int add = Add(5,6);
+		
+		System.out.println("Addition is " + add);
+	}
+
+
+	public int x;
+	public int y;
+	
+	
+	public static  int Add(int x , int y){
+		return x + y;
+	}
+	
+}
