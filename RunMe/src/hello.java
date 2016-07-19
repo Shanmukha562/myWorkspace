b9ab68e6274d6e81a5b60f1c87ea97a969143d28
Sushil Karkee <sushilkarkee@gmail.com>
2016-07-19 22:27:48 +0530

    uygjh

---
+++ RunMe/src/hello.java
@@ -0,0 +1,18 @@
+public class hello {
+public static void main(String[] args) {
+	
+	System.out.println("welcome github");
+	System.out.println("mango is fruit");
+	System.out.println("nai");
+	System.out.println("kasari");
+	System.out.println("hallaBol");
+	System.out.println("desh");
+}
+public static void go(){
+	System.out.println("plz go");
+	//this is from eclipse
+	// hello this is from github
+	
+	
+}
+}
