2c145ddcdfb01f6ac87758fe8efcc48a8b72866d
Sushil Karkee <sushilkarkee@users.noreply.github.com>
2016-07-19 22:14:40 +0530

    Update hello.java

--- RunMe/src/hello.java
+++ RunMe/src/hello.java
@@ -1,7 +1,6 @@
 public class hello {
 public static void main(String[] args) {
-	System.out.println("hello");
-	System.out.println("hiii123");
+	
 	System.out.println("welcome github");
 	System.out.println("mango is fruit");
 	System.out.println("hora");
