cb76de9e148d7ad470b15e5d4964f04ec5ca5d53
Sushil Karkee <sushilkarkee@gmail.com>
2016-07-19 22:08:35 +0530

    sth deleted

--- RunMe/src/hello.java
+++ RunMe/src/hello.java
@@ -4,7 +4,6 @@ public static void main(String[] args) {
 	System.out.println("hiii123");
 	System.out.println("welcome github");
 	System.out.println("mango is fruit");
-	System.out.println("hora");
 	System.out.println("nai");
 	System.out.println("kasari");
 	System.out.println("hallaBol");
