7aa73d5e0848dd1e1a5634a87eefee1e54ced051
Sushil Karkee <sushilkarkee@users.noreply.github.com>
2016-07-20 15:23:37 +0530

    changes made from github - Sushil

--- AmeerAndSushil/src/Common.java
+++ AmeerAndSushil/src/Common.java
@@ -3,6 +3,7 @@ public class Common {
 
 	public void trySomeChange(){
 		System.out.println("This file is for experiment for merge issue from multiple developer");
+		System.out.println("changes made from github from sushil Account")
 	}
 	
 	
