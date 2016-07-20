baa3841006d05a112a92261c6dd97f59b4a35f36
sushil.bahadur.karki <sushil.bahadur.karki@HDC4-D-3B7ZXC2.dir.svc.accenture.com>
2016-07-20 15:46:05 +0530

    javaDocs added for Calculator class

--- AmeerAndSushil/src/Calculator.java
+++ AmeerAndSushil/src/Calculator.java
@@ -1,16 +1,34 @@
-
+/**
+ * Some basic operations in calculator
+ * @author sushil.bahadur.karki
+ *
+ */
 public class Calculator {
 
+	/**
+	 * Two variable function
+	 */
 	public int  x;
 	public int y;
 	
-	
+	/**
+	 * Return addition of two number
+	 * @param firstNumber
+	 * @param secondNumber
+	 * @return firstNubmer + secondNumber
+	 */
 	public int Addition(int firstNumber, int secondNumber){
 		x = firstNumber;
 		y = secondNumber;
 		return x + y;
 	}
 	
+	/**
+	 * Returns subtraction between two numbers
+	 * @param firstNumber
+	 * @param secondNumber
+	 * @return firstNumber - secondNubmer
+	 */
 	public int Subraction(int firstNumber, int secondNumber){
          x = firstNumber;
          y = secondNumber;
