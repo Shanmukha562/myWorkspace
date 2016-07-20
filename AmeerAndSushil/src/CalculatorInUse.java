b3c3b71d97a31a74906d9d281fb33e72906d1d01
sushil.bahadur.karki <sushil.bahadur.karki@HDC4-D-3B7ZXC2.dir.svc.accenture.com>
2016-07-20 16:55:26 +0530

    Class Implementation added for calculator

---
+++ AmeerAndSushil/src/CalculatorInUse.java
@@ -0,0 +1,11 @@
+/**
+ * This class implements Calculator function
+ * @author sushil.bahadur.karki
+ *
+ */
+public class CalculatorInUse {
+public static void main(String[] args) {
+       System.out.println("Addition for first number and second number is " + Calculator.Addition(10, 5));
+	
+}
+}
