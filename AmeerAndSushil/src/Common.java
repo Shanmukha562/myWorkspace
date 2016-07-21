f1843cc9c27f04644fa537cdda1c77760ad251d4
sushil.bahadur.karki <sushil.bahadur.karki@HDC4-D-3B7ZXC2.dir.svc.accenture.com>
2016-07-21 14:56:30 +0530

    added

--- AmeerAndSushil/src/Common.java
+++ AmeerAndSushil/src/Common.java
@@ -8,7 +8,7 @@ public class Common {
 	public void trySomeChange(){
 		System.out.println("This file is for experiment for merge issue from multiple developer");
 		System.out.println("changes made from github from amer Account");
+		System.out.println("added new line by Sushil");
+        
 	}
-	
-	
 }
